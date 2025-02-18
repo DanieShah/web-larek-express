05ee0d8f604bb818820a79081173ee9df6841231
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-02-18 15:45:45 +0300

    Проверка прохождения теста

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek';
+export const DB_ADDRESS = process.env.DB_ADDRESS as string;
