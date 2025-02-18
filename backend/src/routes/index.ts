095184260e6a3c99c5331c440a69c5ee9b151c58
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-02-18 17:31:06 +0300

    Проверка статуса

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,20 @@
-import { Router } from 'express';
+import {
+  NextFunction,
+  Request,
+  Response,
+  Router,
+} from 'express';
 import productRouters from './products';
 import orderRouter from './order';
+import NotFoundError from '../errors/not-found-error';
 
 const router = Router();
 
 router.use('/product', productRouters);
 router.use('/order', orderRouter);
+
+router.use((_req: Request, _res: Response, next: NextFunction) => {
+  next(new NotFoundError('Запрос не сущетсвует'));
+});
 
 export default router;
