a19c3b8d3593d1098ec1ced7589d399382008853
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-02-18 16:41:37 +0300

    Проверка

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,2 +1,2 @@
 export const { PORT = 3000 } = process.env;
-export const { DB_ADDRESS = "mongodb://127.0.0.1:27017/weblarek"} = process.env;
+export const { DB_ADDRESS = "mongodb://127.0.0.1:27017/weblarek" } = process.env;
