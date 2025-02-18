e917b214d5ef593ac1d4a485b59b6879c2af2643
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-02-18 16:06:25 +0300

    Проверка PORT

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT;
 export const DB_ADDRESS = process.env.DB_ADDRESS as string;
