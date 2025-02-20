0dd70d324dbd401d3595c301bcf5990ab7907d7d
Daniel Shahovtsov <daniil.shahovtsov@yandex.ru>
2025-02-20 17:33:05 +0300

    Исправление после ревью

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-export const { PORT, DB_ADDRESS } = process.env;
+export const { PORT = 3000, DB_ADDRESS = 'mongodb://root:example@mongo:27017/' } = process.env;
--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -4,38 +4,38 @@ import Product from '../models/products';
 import BadRequestError from '../errors/bad-request-error';
 
 const postOrder = async (req: Request, res: Response, next: NextFunction) => {
-  const order = req.body;
+  try {
+    const order = req.body;
+    const { total, items, payment } = order;
+    const productBase = await Product.find({});
+    let totalPrice: number = 0;
 
-  const { total, items, payment } = order;
+    items.forEach((id: string) => {
+      const productForOrder = productBase.find((prod) => String(prod._id) === id);
+      if (!productForOrder) {
+        return next(new BadRequestError('Товара с данным id не существует'));
+      }
+      totalPrice += productForOrder.price;
+      return totalPrice;
+    });
 
-  const productBase = await Product.find({});
-  let totalPrice: number = 0;
-
-  items.forEach((id: string) => {
-    const productForOrder = productBase.find((prod) => String(prod._id) === id);
-    if (!productForOrder) {
-      return next(new BadRequestError('Товара с данным id не существует'));
+    if (payment !== 'online') {
+      if (payment !== 'card') {
+        return next(new BadRequestError('Неправильно выбран тип оплаты'));
+      }
     }
 
-    totalPrice += productForOrder.price;
-
-    return totalPrice;
-  });
-
-  if (payment !== 'online') {
-    if (payment !== 'card') {
-      return next(new BadRequestError('Неправильно выбран тип оплаты'));
+    if (totalPrice !== total) {
+      return next(new BadRequestError(`Сумма указанная в запросе ${totalPrice} не совпадает с ${total}`));
     }
-  }
 
-  if (totalPrice !== total) {
-    return next(new BadRequestError(`Сумма указанная в запросе ${totalPrice} не совпадает с ${total}`));
+    return res.status(200).send({
+      id: faker.string.uuid(),
+      total,
+    });
+  } catch (err) {
+    return next(err);
   }
-
-  return res.status(200).send({
-    id: faker.string.uuid(),
-    total,
-  });
 };
 
 export default postOrder;
